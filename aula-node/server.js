dcf1121789448f054920fa49b36c9fb08daf48ed
Victor Matheus de Moraes e Silva <victor.moraes@softdesign.com.br>
2019-06-17 13:18:19 -0300

    ultima aula

---
+++ aula-node/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+//Iniciando o app
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Iniciando o db
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+requireDir('./src/models');
+
+//const Product = mongoose.model('Product');
+
+//Rota coringa, recebe e encaminha p routes
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
